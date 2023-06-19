10609e1213f7f9b04bc79a710e7b6371a118d428
Amit <amitmaurya15000@gmail.com>
2023-06-20 00:50:55 +0530

    Intial Commit

---
+++ app.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const express = require('express') 
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption')
+const ejs = require('ejs')
+const app = express()
+
+app.use(express.static("public"))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+ 
+mongoose.connect('mongodb://localhost:27017/userDB');
+
+const userSchema = new mongoose.Schema({
+  email : String,
+  password : String
+});
+const secret = "thisIsMyLittleSecret";
+
+userSchema.plugin(encrypt, { secret :process.env.SECRET, encryptedFields:["password"]});
+
+
+const User = new mongoose.model("User", userSchema)
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+
+app.get('/login', (req, res) => {
+    res.render('login')
+  })
+
+  
+app.get('/register', (req, res) => {
+    res.render('register')
+  })
+
+app.post('/register', (req,res)=>{
+  const newUser = new User({
+    email : req.body.username,
+    password: req.body.password
+  })
+  newUser.save()
+  .then(savedUser => {
+    res.render('secrets')
+  })
+  .catch(error => {
+    console.error(error);
+})
+});
+
+
+app.post('/login', (req,res)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  User.findOne({email:username})
+  .then((foundUser=>{
+    if(foundUser.password === password){
+      res.render('secrets')
+    }
+    else{res.send("<h1>Enter correct Email and Password.</h1><br>Please register before login")}
+  }))
+  .catch(err => res.render("There occurred some error"+ err))
+  });
+
+app.listen('3000', ()=>{console.log("Server started on port 3000")} );
