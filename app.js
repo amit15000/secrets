97ccafc84bced71dc48588b151deeb6dce73247b
Amit Kumar <15000amitkumar@gmail.com>
2023-11-03 03:40:49 +0530

    Done

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post('/register', (req,res)=>{
 })
 });
 
-AMITTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+
 
 app.post('/login', (req,res)=>{
   const username = req.body.username;
